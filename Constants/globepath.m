afc907a0bcae6d4dc99ba8886973db6f5b3a30fc
Josh Laughner <joshlaugh5@gmail.com>
2015-01-20 10:12:06 -0800

    globepath.m updated to use new file server

--- Constants/globepath.m
+++ Constants/globepath.m
@@ -3,7 +3,7 @@ function [ globe_dir ] = globepath( )
 %   NOTE: This is not a dynamic path; it will need to be updated in this
 %   function if the globe files are moved.
 
-globe_dir = '/Volumes/share/GROUP/SAT/BEHR/GLOBE_files';
+globe_dir = '/Volumes/share-sat/SAT/BEHR/GLOBE_Database';
 
 end
 
