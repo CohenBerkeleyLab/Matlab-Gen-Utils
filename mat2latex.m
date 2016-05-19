32a12cd7b92259fe6a4632693a24cfd96a190a54
Josh Laughner <joshlaugh5@gmail.com>
2016-05-19 10:44:00 -0700

    Fix mat2latex to handle scientific notation.

--- mat2latex.m
+++ mat2latex.m
@@ -24,31 +24,41 @@ end
 fstr1 = sprintf('%s & ', format_spec);
 fstr1b = '%s & ';
 % The end-of-line print string
-fstr2 = sprintf('%s \\\\\\\\ \n', format_spec);
-fstr2b = '%s \\\\ \n';
-% The end of line print string for the last line
-fstr3 = sprintf('%s \n', format_spec);
-fstr3b = '%s \n';
+fstr2 = sprintf(tex_in_printf('%s \ \n',3), format_spec);
+fstr2b = tex_in_printf('%s \ \n',3);
+
 
 sz = size(M);
 for a=1:sz(1)
     for b=1:sz(2)
         if isnumeric(M{a,b})
             if b < sz(2)
-                fprintf(fstr1, M{a,b});
-            elseif a < sz(1)
-                fprintf(fstr2, M{a,b});
+                fstr=sprintf(fstr1, M{a,b});
             else
-                fprintf(fstr3, M{a,b});
+                fstr=sprintf(fstr2, M{a,b});
             end
+            % Replace e notation with x10^, remove + sign and leading 0s
+            fstr = regexprep(fstr,'e',' \\times 10^{');
+            fstr = regexprep(fstr,'(?<={-?)+?0*(?=\d+)','');
+            %fstr = regexprep(fstr,'(?<={)\+?0*(?=\d+)','');
+            fstr = regexprep(fstr,'(?<={-?\d+) (?=&|\\)','}$ ');
+            if ~isempty(strfind(fstr,'{')) && isempty(strfind(fstr,'}'))
+                fstr = strcat(fstr,'}$'); % handles the final line
+            end
+            % Numbers without an exponent will not have the ending $ added
+            % yet
+            if isempty(strfind(fstr,'$'))
+                fstr = regexprep(fstr,' (?=&|\\)','$ ');
+            end
+            fprintf('$%s',fstr);
         else
             if b < sz(2)
                 fprintf(fstr1b, M{a,b});
-            elseif a < sz(1)
-                fprintf(fstr2b, M{a,b});
             else
-                fprintf(fstr3b, M{a,b});
+                fprintf(fstr2b, M{a,b});
             end
         end
     end
 end
+
+fprintf('\n')
