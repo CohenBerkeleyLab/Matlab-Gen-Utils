b8f48aad08f10a01a5b2e1169a287153ce882ba7
Josh Laughner <joshlaugh5@gmail.com>
2016-08-25 17:03:47 -0700

    behr_dir function added and seq_mat can now be any number of dimensions.

--- seq_mat.m
+++ seq_mat.m
@@ -1,15 +1,11 @@
 %This function will return a matrix of the specified dim'ns with each entry
 %labeled with its sequential index.
 
-function matrix_out = seq_mat(x,y,z)
-if nargin < 2
-    y = 1;
-    z = 1;
-elseif nargin < 3
-    z = 1;
-end
-matrix_out = zeros(x,y,z);
+function matrix_out = seq_mat(varargin)
 
-for a=1:x*y*z
+matrix_out = zeros(varargin{:});
+for a=1:numel(matrix_out)
     matrix_out(a) = a;
+end
+
 end
